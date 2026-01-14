480c8354c798ff42f19c8fdf1afe17eccf98f6e1
TanishRadhakrishna <tanish.is22@sahyadri.edu.in>
2026-01-14 10:52:16 +0530

    Updates:Backend fix

--- backend/src/routes/predict.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import upload from "../utils/upload.js";
-import { analyzeImage } from "../controllers/predict.controller.js";
-
-const router = express.Router();
-
-router.post("/", upload.single("image"), analyzeImage);
-
-export default router;
--- backend/src/services/ai.service.js
+++
@@ -1,6 +0,0 @@
-import dummyResults from "../data/dummyResults.js";
-
-export function getDummyPrediction() {
-  const randomIndex = Math.floor(Math.random() * dummyResults.length);
-  return dummyResults[randomIndex];
-}
